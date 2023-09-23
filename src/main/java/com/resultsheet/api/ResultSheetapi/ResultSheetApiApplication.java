23b4abcf30055a3376991b9bb381c1c5a0eb639d
Janith Madarasinghe <janithpm9991@gmail.com>
2023-09-23 15:54:35 +0530

    project init

---
+++ src/main/java/com/resultsheet/api/ResultSheetapi/ResultSheetApiApplication.java
@@ -0,0 +1,13 @@
+package com.resultsheet.api.ResultSheetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultSheetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultSheetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resultsheet/api/ResultSheetapi/ResultSheetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resultsheet.api.ResultSheetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultSheetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
